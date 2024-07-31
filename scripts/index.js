e14beb5a51ea24082a98a87292a58ba47c8891df
Pavel Tsikhanau <tishka1991@gmail.com>
2024-07-31 17:30:56 +0300

    fix: change some adaptive styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,56 @@
-document.querySelector("#watch-menu").onclick = function () {
-  document.querySelector(".products").scrollIntoView({ behavior: "smooth" });
+document.querySelector('#watch-menu').onclick = function () {
+  document.querySelector('.products').scrollIntoView({ behavior: 'smooth' });
 };
 
-let productsButtons = document.querySelectorAll(".products-button");
+let productsButtons = document.querySelectorAll('.products-button');
 for (let i = 0; i < productsButtons.length; i++) {
   productsButtons[i].onclick = function () {
-    document.querySelector(".order").scrollIntoView({ behavior: "smooth" });
+    document.querySelector('.order').scrollIntoView({ behavior: 'smooth' });
   };
 }
 
-let yourOrder = document.querySelector("#your-order"),
-  yourName = document.querySelector("#your-name"),
-  yourPhone = document.querySelector("#your-phone");
-let orderButton = (document.querySelector(".order-button").onclick =
+let yourOrder = document.querySelector('#your-order'),
+  yourName = document.querySelector('#your-name'),
+  yourPhone = document.querySelector('#your-phone');
+let orderButton = (document.querySelector('.order-button').onclick =
   function () {
     let hasError = false;
 
     [yourOrder, yourName, yourPhone].forEach((item) => {
       if (!item.value) {
-        item.parentElement.style.background = "red";
+        item.parentElement.style.background = 'red';
         hasError = true;
       } else {
-        item.parentElement.style.background = "";
+        item.parentElement.style.background = '';
       }
     });
 
     if (!hasError) {
       [yourOrder, yourName, yourPhone].forEach((item) => {
-        item.value = "";
+        item.value = '';
       });
-      alert("Cпасибо за заказ! Мы скоро с вами свяжемся!");
+      alert('Cпасибо за заказ! Мы скоро с вами свяжемся!');
     }
   });
 
-let prices = document.querySelectorAll(".products-item-price");
+let prices = document.querySelectorAll('.products-item-price');
 
-document.querySelector(".currency").onclick = function (e) {
+document.querySelector('.currency').onclick = function (e) {
   let currentCurrency = e.target.innerText,
-    newCurrency = "$",
+    newCurrency = '$',
     coefficient = 1;
 
-  if (currentCurrency === "$") {
-    newCurrency = "₽";
+  if (currentCurrency === '$') {
+    newCurrency = '₽';
     coefficient = 100;
-  } else if (currentCurrency === "₽") {
-    newCurrency = "BYN";
+  } else if (currentCurrency === '₽') {
+    newCurrency = 'BYN';
     coefficient = 3;
-  } else if (currentCurrency === "BYN") {
-    newCurrency = "€";
+  } else if (currentCurrency === 'BYN') {
+    newCurrency = '€';
     coefficient = 1.1;
-  } else if (currentCurrency === "€") {
-    newCurrency = "¥";
+  } else if (currentCurrency === '€') {
+    newCurrency = '¥';
     coefficient = 6.9;
   }
 
@@ -58,31 +58,29 @@ document.querySelector(".currency").onclick = function (e) {
 
   for (let i = 0; i < prices.length; i++) {
     prices[i].innerText =
-      +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) +
-      " " +
+      +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) +
+      ' ' +
       newCurrency;
   }
 };
 
-
 //slider
 
 const products = document.querySelector('.products-items');
-console.log(products)
+console.log(products);
 
 // const button = document.createElement('button');
 // button.innerText ='button left';
 // products.unshift(button)
 
 window.addEventListener('resize', function () {
-	if (window.innerWidth <= 768) {
-		// 0...768
-      products.classList.remove('products-items');
-      products.classList.add('slider');
-
-	} else {
-		// 769...Inf
-      products.classList.remove('slider');
-      products.classList.add('products-items')
-	}
+  if (window.innerWidth <= 768) {
+    // 0...768
+    products.classList.remove('products-items');
+    products.classList.add('slider');
+  } else {
+    // 769...Inf
+    products.classList.remove('slider');
+    products.classList.add('products-items');
+  }
 });
